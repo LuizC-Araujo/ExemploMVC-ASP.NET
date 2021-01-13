d5e5e261142d60f76bafb5fa2ff3a76889b2b50b
Luiz Araujo <arauldesign@gmail.com>
2021-01-13 20:34:03 -0300

    Exemplo MVC com ASP.NET e Entity Framework

---
+++ ExemploMVC/Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploMVC.Models
+{
+    public class Categoria
+    {
+        public int id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo é obrigatório")]
+        public string descricao { get; set; }
+        public List<Produto> Produtos { get; set; }
+    }
+}
---
+++ ExemploMVC/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O Campo é obrigatório!")]
+        public string Descricao { get; set; }
+        [Required(ErrorMessage = "O Campo é obrigatório!")]
+        [Range(1, 100, ErrorMessage = "Valor fora dos limites de 1 a 100")]
+        public int Quantidade { get; set; }
+        public int CategoriaId{ get; set; }
+        public Categoria Categoria{ get; set; }
+    }
+}
